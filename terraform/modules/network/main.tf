ee843dfa24b8c116d6872301f00da7481d70ba46
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2025-08-05 11:28:16 +0530

    update the bugs

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,18 +1,19 @@
-resource "aws" "main" {
-    cidr_block = var.vpc_cidr
-    enable_dns_support = true
-    enable_dns_hostname = true
-    tags = {
-        Name = "devops-vpc"
-        Project = "var.project_name"
-    }
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = { 
+    Name = "devops-vpc" 
+    Project     = var.project_name
+  }
 }
 
 resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc.main.id
-    tags = {
-        Name = "${var.project_name}-igw"
-        Project     = var.project_name
+  vpc_id = aws_vpc.main.id
+
+  tags = { 
+    Name = "${var.project_name}-igw"
+    Project     = var.project_name
     }
 }
 
